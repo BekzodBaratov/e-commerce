3e39f770220801a6046987e5deeaacdda229b5c4
Bekzod <bekzodbaratov334@gmail.com>
2023-02-04 13:22:52 +0500

    fix

--- src/components/LoadingModal.vue
+++ src/components/LoadingModal.vue
@@ -2,12 +2,8 @@
   <Teleport to="body">
     <div class="fixed z-[999999] inset-0 bg-[rgba(0,0,0,0.4)] backdrop-blur-sm">
       <div class="fixed z-[999999] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <img :src="LoadingSvg" alt="loading svg" />
+        <img src="../assets/icons/Infinity_loading.svg" alt="loading_svg" />
       </div>
     </div>
   </Teleport>
 </template>
-
-<script setup lang="ts">
-import LoadingSvg from "../assets/icons/Infinity-loading.svg"
-</script>
